9597059da8e36762c3601a68d714ef3bf6818ecd
CarolineVeleda <carollimaveleda@yahoo.com.br>
2018-08-16 15:52:45 -0300

    Add files via upload

---
+++ extras.sql
@@ -0,0 +1,40 @@
+﻿
+
+--1) Crie uma view que liste os dados de fornecedores (nomeFantasia, número de vendas para sua loja, número de produtos já
+--vendidos).
+
+
+select forn."nomeFantasia", count(distinct com."codFornecedor")
+	from "Fornecedor" forn left outer join "Compra" com
+	on forn."cod"=com."codFornecedor" 
+	group by forn."nomeFantasia"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+select * from notafiscal
+select * from funcionario
+select * from cliente
+select * from itemvenda
+select * from departamento
+select * from produto
+
+select * from "Compra"
+select * from "Fornecedor"
+select * from "ItemCompra"
+
+
+
+
+	
